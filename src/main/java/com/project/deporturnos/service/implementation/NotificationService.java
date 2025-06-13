8671dcac70603f30b7c356a781f152e525e358c0
Pulpoide <joacolivero@gmail.com>
2025-06-13 02:41:43 -0300

    Fix: Comment out unused method to resolve warnings
    Commented out an unused method to eliminate associated compiler warnings and improve code cleanliness.

--- src/main/java/com/project/deporturnos/service/implementation/NotificationService.java
+++ src/main/java/com/project/deporturnos/service/implementation/NotificationService.java
@@ -1,6 +1,5 @@
 package com.project.deporturnos.service.implementation;
 
-import com.project.deporturnos.entity.domain.Turno;
 import com.project.deporturnos.entity.domain.Usuario;
 import com.project.deporturnos.service.INotificationService;
 import com.project.deporturnos.utils.QRCodeGenerator;
@@ -11,7 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -70,38 +68,38 @@ public class NotificationService implements INotificationService {
         }
     }
 
-    public void notifyUsersAboutPromotions(List<Usuario> usuariosANotificar, Turno turno, Usuario usuarioQueCancela){
-        String subject = "Turno disponible en promoción ⚽";
-
-        String url = "http://localhost:5173/turnos-disponibles/" + turno.getCancha().getId();
-
-        String body = "<html>"
-                + "<body style=\"font-family: Arial, sans-serif;\">"
-                + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
-                + "<h2 style=\"color: #333;\">¡Turno " + turno.getId() + " disponible!</h2>"                + "<p style=\"font-size: 16px;\">Se ha liberado un turno en la cancha: " + turno.getCancha().getNombre() + ".</p>"
-                + "<p style=\"font-size: 16px;\">Fecha: " + turno.getFecha() + "</p>"
-                + "<p style=\"font-size: 16px;\">Hora de inicio: " + turno.getHoraInicio() + "</p>"
-                + "<p style=\"font-size: 16px;\">Hora de fin: " + turno.getHoraFin() + "</p>"
-                + "<p style=\"font-size: 16px;\">¡Aprovecha el descuento especial por cancelación!</p>"
-                + "<a href=\"" + url + "\" style=\"display: inline-block; background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;\">Ver turno disponible</a>"
-                + "</div>"
-                + "</body>"
-                + "</html>";
-
-        // Excluimos al usuario que realizó la cancelación
-        List<Usuario> usuariosFiltrados = usuariosANotificar.stream()
-                .filter(u -> !u.getId().equals(usuarioQueCancela.getId()))
-                .toList();
-
-        // Iteramos sobre los usuarios a notificar
-        for (Usuario usuario : usuariosFiltrados) {
-            try {
-                emailService.sendEmail(usuario.getEmail(), subject, body);
-                System.out.println("Notificación enviada a: " + usuario.getEmail());
-            } catch (Exception e) {
-                System.err.println("Error al enviar notificación a: " + usuario.getEmail());
-                e.printStackTrace();
-            }
-        }
-    }
+//    public void notifyUsersAboutPromotions(List<Usuario> usuariosANotificar, Turno turno, Usuario usuarioQueCancela){
+//        String subject = "Turno disponible en promoción ⚽";
+//
+//        String url = "http://localhost:5173/turnos-disponibles/" + turno.getCancha().getId();
+//
+//        String body = "<html>"
+//                + "<body style=\"font-family: Arial, sans-serif;\">"
+//                + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
+//                + "<h2 style=\"color: #333;\">¡Turno " + turno.getId() + " disponible!</h2>"                + "<p style=\"font-size: 16px;\">Se ha liberado un turno en la cancha: " + turno.getCancha().getNombre() + ".</p>"
+//                + "<p style=\"font-size: 16px;\">Fecha: " + turno.getFecha() + "</p>"
+//                + "<p style=\"font-size: 16px;\">Hora de inicio: " + turno.getHoraInicio() + "</p>"
+//                + "<p style=\"font-size: 16px;\">Hora de fin: " + turno.getHoraFin() + "</p>"
+//                + "<p style=\"font-size: 16px;\">¡Aprovecha el descuento especial por cancelación!</p>"
+//                + "<a href=\"" + url + "\" style=\"display: inline-block; background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;\">Ver turno disponible</a>"
+//                + "</div>"
+//                + "</body>"
+//                + "</html>";
+//
+//        // Excluimos al usuario que realizó la cancelación
+//        List<Usuario> usuariosFiltrados = usuariosANotificar.stream()
+//                .filter(u -> !u.getId().equals(usuarioQueCancela.getId()))
+//                .toList();
+//
+//        // Iteramos sobre los usuarios a notificar
+//        for (Usuario usuario : usuariosFiltrados) {
+//            try {
+//                emailService.sendEmail(usuario.getEmail(), subject, body);
+//                System.out.println("Notificación enviada a: " + usuario.getEmail());
+//            } catch (Exception e) {
+//                System.err.println("Error al enviar notificación a: " + usuario.getEmail());
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
